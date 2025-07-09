0627321ab64b4f0e11bcbf37d145fb4820d69a0a
Abdullah_assoli <90969574+ASSOLI99@users.noreply.github.com>
2025-07-09 19:50:34 +0300

    Revert "ready"
    This reverts commit d136deaeb8f2b6c034b23be1ce6e388fed6eac7b.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,10 +16,11 @@ const Contact = () => {
           
           <div className='absolute -left-20 top-0 hidden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96'>
               <ImageClipBox src="/img/contact-1.webp" clipClass="contact-clip-path-1" />
-              <ImageClipBox src="/img/contact-2.webp" clipClass="hidden md:block contact-clip-path-2 -translate-y-64 translate-x-42" />
+              <ImageClipBox src="/img/contact-2.webp" clipClass="contact-clip-path-2 lg:translate-y-40 translate-y-60" />
           </div>
 
-          <div className='absolute -top-5/12 left-20 w-60 sm:top-1/2 md:left-auto md:right-10 lg:top-20 lg:w-80'>
+          <div className='absolute -top-40 left-20 w-60 sm:top-1/2 md:left-auto md:right-10 lg:top-20 lg:w-80'>
+              <ImageClipBox src="/img/swordman-partial.webp" clipClass="absolute md:scale-125" />
               <ImageClipBox src="/img/swordman.webp" clipClass="sword-man-clip-path md:scale-125" />
           </div>
 
